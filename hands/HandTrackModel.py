a7c75b3705f52b36f4254aa3761a2696c9b95c0c
Isaac-Muscat <isaac.wieting.muscat@gmail.com>
2021-04-23 21:12:11 -0400

    Added hands directory

--- hands/HandTrackModel.py
+++ hands/HandTrackModel.py
@@ -11,8 +11,10 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 def get_hand_position(frame):
+    frame = cv2.flip(frame, 1)
+    camRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     hand_list = []
-    results = hands.process(frame)
+    results = hands.process(camRGB)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             hand_list.append(handLms.landmark)
