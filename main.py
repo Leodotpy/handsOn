64688db47a74b9322022dfcedfc8c7d0f17e3083
Loochsi <lucas.disarra@gmail.com>
2021-04-24 00:18:16 -0500

    Lucas- Right Face Draw

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ from Physics import Ball as B
 import time
 import WinGUI
 import pygame
+from pingPong.constants import Constants as c
 
 width, height = 1920, 1080
 gravity = M.Vector3(0, -9.8, 0)
 
-bounds = [M.BoundRect(M.Vector3(-500, -500, 100), M.Vector3(500, 500, 1100), True)]
+bounds = [M.BoundRect(M.Vector3(-500, -500, 0), M.Vector3(500, 500, 1000), M.Vector3(0, 0, 0), True)]
 
-player_paddle = M.BoundRect(M.Vector3(-250, 0, 100), M.Vector3(-100, 100, 101), False)
-ai_paddle = M.BoundRect(M.Vector3(-250, 0, 1100), M.Vector3(-100, 100, 1099), False)
+player_paddle = M.BoundRect(M.Vector3(-200, -200, 0), M.Vector3(200, 200, 100), M.Vector3(0, 0, 50), False)
+ai_paddle = M.BoundRect(M.Vector3(-200, -200, 900), M.Vector3(200, 200, 1000), M.Vector3(0, 0, 950), False)
 
 bounds.append(player_paddle)
 bounds.append(ai_paddle)
 
 ball = B.Ball(50, M.Vector3(0, 100, 0), M.Vector3(100, 100, 100), bounds)
-
 w = WinGUI.DrawableWin(ball)
 
 running = True
 
 lastTime = time.time()
+prevMouseX, prevMouseY = pygame.mouse.get_pos()
 while running:
     # Calculate delta time in seconds
     currentTime = time.time()
@@ -30,11 +31,16 @@ while running:
 
     ball.PhysicsTick(deltaTime)
 
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    mouseX, mouseY = pygame.mouse.get_pos()
+
+    deltaBlock = M.Vector3(mouseX - c.halfDims[0], c.windowDims[1]-mouseY-c.halfDims[1], 50)
+    ball.bounds[1].moveBlock(deltaBlock)
+
     w.drawFrame()
+
 
 pygame.quit()
