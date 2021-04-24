b21a3ba1508f15e9bbf9fbace6b46a6a7f0a2d45
Isaac-Muscat <isaac.wieting.muscat@gmail.com>
2021-04-23 21:33:43 -0400

    Added hands directory

--- hands/HandTrackModel.py
+++ hands/HandTrackModel.py
@@ -8,8 +8,10 @@ import time
 import pyautogui as mouse
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+hands = mpHands.Hands(max_num_hands=2)
 mpDraw = mp.solutions.drawing_utils
+
+#Gets hand positions --> returns list: hand_list[integer: hand
 def get_hand_position(frame):
     frame = cv2.flip(frame, 1)
     camRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -19,3 +21,13 @@ def get_hand_position(frame):
         for handLms in results.multi_hand_landmarks:
             hand_list.append(handLms.landmark)
     return hand_list
+
+#Returns a cleaned point into an integer value (x,y,z)
+#Input: point = (x, y, z), w/h = w and h of frame, mons = window width/height
+def clean_point_to_int(point, w, h, monWidth, monHeight):
+    offsetX = int(w / 10)
+    offsetY = int(h / 8)
+    x = (point[0] - offsetX) / (w - (offsetX * 2)) * monWidth
+    y = (point[1] - offsetY) / (h - (offsetY * 2)) * monHeight
+    z = point[2]
+    return (x, y, z)
