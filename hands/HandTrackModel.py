13d0b81236e9c8ca324ccaf206db7f221f87280d
Isaac-Muscat <isaac.wieting.muscat@gmail.com>
2021-04-23 21:08:17 -0400

    Added hands directory

--- hands/HandTrackModel.py
+++ hands/HandTrackModel.py
@@ -10,32 +10,10 @@ import pyautogui as mouse
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-monWidth, monHeight = mouse.size()
-
-def create_video_stream():
-    cap = cv2.VideoCapture(1)
-
-    while (True):
-        ret, frame = cap.read()
-
-        frame = cv2.flip(frame, 1)
-
-        camRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = hands.process(camRGB)
-
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                w, h, c = frame.shape
-                cx = int(handLms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * w)
-                cy = int(handLms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * h)
-                cz = handLms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].z
-
-                mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
-
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+def get_hand_position(frame):
+    hand_list = []
+    results = hands.process(frame)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            hand_list.append(handLms.landmark)
+    return hand_list
