eea79a52093a19ea49d52d71057fa611fe451a17
Loochsi <lucas.disarra@gmail.com>
2021-04-23 20:54:06 -0500

    Lucas- removed duplicate main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import cv2
+import Physics.Math as m
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    camera = cv2.VideoCapture(0)
-    while True:
-        print("hi")
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+gravity = m.Vector3(0, -9.8, 0)
